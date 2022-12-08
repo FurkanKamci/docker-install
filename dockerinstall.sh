e53263025b38d95dd9c74c8e8a9441744cc7a3f5
FurkanKamci <37150333+FurkanKamci@users.noreply.github.com>
2022-12-08 10:16:50 +0300

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -16,8 +16,9 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io -y
+
 sudo usermod -aG docker $USER
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.14.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 sudo systemctl start docker
